5fc79b499d4c32ee73ea8f566c7ee38cbb329ab4
cydu <root@cydu.net>
2014-07-08 15:41:49 +0800

    tweet sentiment

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,17 +1,28 @@
+# -*- coding: utf-8 -*-
 import sys
+import json
+import re
 
-def hw():
-    print 'Hello, world!'
-
-def lines(fp):
-    print str(len(fp.readlines()))
+def process(sent_file, tweet_file):
+    scores = {}
+    for line in sent_file:
+        term, score  = line.split("\t")
+        scores[term] = int(score)
+    
+    for line in tweet_file:
+        score = 0
+        try:
+            tweet = json.loads(line)
+            words = re.findall(r"[a-zA-Z]+",tweet['text'])
+            score = sum([scores.get(w.lower(), 0) for w in words])
+        except:
+            pass
+        print score
 
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    process(sent_file, tweet_file)
 
 if __name__ == '__main__':
     main()
